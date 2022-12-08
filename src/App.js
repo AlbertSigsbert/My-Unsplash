1576f94d1453187293058bc2cea9ee10e0095feb
Albert <ishengomaalbert@gmail.com>
2022-12-08 09:23:42 +0300

    Created Sample Img Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Grid from "./components/Grid";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="mx-[6%]">
        <Header/>
+       <Grid />
     </div>
   );
 }
